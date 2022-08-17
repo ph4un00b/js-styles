3f590526e28bd4ca76662c8517a2b9a28e692bb6
ph4un00b <faunotv@gmail.com>
2022-08-17 18:28:43 -0500

    1 file modified, deno unhandled rejections

--- errors.js
+++ errors.js
@@ -19,17 +19,37 @@ http
   })
   .listen(5000);
 
-// https://medium.com/deno-the-complete-reference/unhandled-rejection-denos-equivalent-of-node-js-12d2fce1418
-// DENO does not have global handlers at the moment!
+// DENO does not have a global handlers for exeptions
+// but unhandled rejections!
 
-// const listener = Deno.listen({ port: 5000 });
-// for await (const conn of listener) {
-//   for await (const { request: req, respondWith: res } of Deno.serveHttp(conn)) {
-//     try {
-//       const d = await req.json();
-//       res(new Response());
-//     } catch (e) {
-//       res(new Response(null, { status: 400 }));
-//     }
-//   }
-// }
+denoland: {
+  const listener = Deno.listen({ port: 5000 });
+  for await (const conn of listener) {
+    for await (const { request: req, respondWith: res } of Deno.serveHttp(
+      conn
+    )) {
+      try {
+        const d = await req.json();
+        res(new Response());
+      } catch (e) {
+        res(new Response(null, { status: 400 }));
+      }
+    }
+  }
+}
+
+denoland: {
+  import { serve } from 'https://deno.land/std/http/mod.ts';
+
+  globalThis.addEventListener('unhandledrejection', (e) => {
+    console.log(Date.now() + ':' + e.reason); // 1660629861046:NotFound: No such file or directory (os error 2)
+    // this code below will prevent exits!
+    e.preventDefault();
+  });
+
+  setInterval(async () => {
+    const file = await Deno.readTextFile('./non-existent.txt');
+    console.log('FILE:', file);
+  }, 5000);
+  serve(() => new Response('!!'), { port: 8080 });
+}
